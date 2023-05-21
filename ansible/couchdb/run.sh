9069ec3bb77090b8cdf87d26cc9fc1f903305329
feng <fjii2333@gmail.com>
2023-05-21 14:02:15 +1000

    .

---
+++ ansible/couchdb/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ansible-galaxy collection install openstack.cloud:1.10.0  # install openstack collection
+ansible-galaxy collection install community.docker
+. ./openrc.sh; ansible-playbook playbook.yaml -i inventory/inventory --ask-become-pass
+
+
